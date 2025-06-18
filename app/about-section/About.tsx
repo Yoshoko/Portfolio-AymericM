18ef8775a9a93e364a27bd57a458db36d27f873f
AymericMartinez <aymeric.martinez@esd-mail.com>
2025-06-18 12:49:07 +0200

    feat change text

--- app/about-section/About.tsx
+++ app/about-section/About.tsx
@@ -13,7 +13,7 @@ const APropos = () => {
       <div className="mx-auto flex w-[90%] flex-col items-center justify-center lg:max-w-[1212.8px]">
         <AnimatedTitle
           text={
-            "J’AIME CRÉER, DESIGNER, DÉVELOPPER ET DONNER VIE À DES EXPÉRIENCES VISUELLES QUI RACONTENT ET INSPIRENT."
+            "J’AIME CRÉER, DESIGNER, DÉVELOPPER ET DONNER VIE À DES EXPÉRIENCES."
           }
           className={
             "mb-10 text-left text-[40px] font-bold leading-[0.9em] tracking-tighter text-[#e4ded7] sm:text-[45px] md:mb-16 md:text-[60px] lg:text-[80px]"
